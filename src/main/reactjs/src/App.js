c6395e8baf7af39e783b3171d9d79b6e114cb25b
wontae chang <cwt629@naver.com>
2024-01-26 16:06:20 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import mainImg from './image/testforpublish.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={mainImg} />
+      <h2><b>TODAC 프로젝트 떡상 드가자</b></h2>
+    </div>
+  );
+}
+
+export default App;
