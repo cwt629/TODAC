807b6c19e8d78d1311ce2295215beaf02f2d8de2
2Smean <136636643+2Smean@users.noreply.github.com>
2024-01-30 17:47:34 +0900

    test lsm

--- src/main/java/com/example/demo/TodacApplication.java
+++ src/main/java/com/example/demo/TodacApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodacApplication {
 
+	// git test  240130 05:46
 	public static void main(String[] args) {
 		SpringApplication.run(TodacApplication.class, args);
 	}
