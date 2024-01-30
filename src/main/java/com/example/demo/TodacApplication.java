f2485f39a0fe22a0b4036a60ab39fa53b07799ba
joyzoey <skgp9150@gmail.com>
2024-01-30 17:29:02 +0900

    feat : #테스트 김나혜 240130

--- src/main/java/com/example/demo/TodacApplication.java
+++ src/main/java/com/example/demo/TodacApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodacApplication {
 
+	
+	//git test-nahye 240130
 	public static void main(String[] args) {
 		SpringApplication.run(TodacApplication.class, args);
 	}
