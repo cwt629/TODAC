e62e33bf677d980b84f12daa5ac496c6709c1eb1
mongdamhwa <124032622+mongdamhwa@users.noreply.github.com>
2024-01-30 18:17:11 +0900

    test /한충희2

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <img alt='' src={mainImg} />
-      {/* 충희 주석 체크 */}
+      {/* 충희 주석 체크 2*/}
       <h2><b>TODAC 프로젝트 떡상 드가자!!</b></h2>
       <h1><b>이거 배포도 성공하자!!!!!!!!!!!</b></h1>
     </div>
