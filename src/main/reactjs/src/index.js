baf17d259b1c4efd9bf2ebab2d68299e227df73e
mongdamhwa <124032622+mongdamhwa@users.noreply.github.com>
2024-01-30 18:22:13 +0900

    test #테스트 한충희 [240130]

--- src/main/reactjs/src/index.js
+++ src/main/reactjs/src/index.js
@@ -11,7 +11,6 @@ root.render(
     
   </React.StrictMode>
 );
-//한충희 주석테스트
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
