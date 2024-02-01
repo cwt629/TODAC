f5929c0cb0340f79e09d8cddf814c4885d376241
mongdamhwa <124032622+mongdamhwa@users.noreply.github.com>
2024-02-01 14:43:58 +0900

    design #footer아이콘hover색상 한충희 [240201] -아이콘 호버시 색상변경되도록 변경

--- src/main/reactjs/src/Footer.js
+++ src/main/reactjs/src/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { ReactComponent as Community_ico} from "./image/ico_community.svg"
+import { ReactComponent as Chat_ico} from "./image/ico_chat.svg"
+import { ReactComponent as Mypage_ico} from "./image/ico_mypage.svg"
 const Footer = () => {
     const nav = useNavigate();
 
     return (
         <div className='footer fixed-bottom text-center align-items-center'>
             <div className='footernav col'
-            onClick={() => nav('/user/chat')}><img alt='chat' src={require("./image/ico_chat.png")}/></div>
+            onClick={() => nav('/user/chat')}><Chat_ico className='footer_ico'/></div>
             <div className='footernav col'
-            onClick={() => nav('/user')}><img alt='chat' src={require("./image/ico_mypage.png")}/></div>
+            onClick={() => nav('/user')}><Mypage_ico className='footer_ico'/></div>
             <div className='footernav col'
-            onClick={() => nav('/user/community')}><img alt='chat' src={require("./image/ico_community.png")}/></div>
+            onClick={() => nav('/user/community')}><Community_ico className='footer_ico'/></div>
         </div>
     );
 };
