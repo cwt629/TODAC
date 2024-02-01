84fa18a72e234df4ff2f95832a4a1de48149a35c
Nahye <143480649+joyzoey@users.noreply.github.com>
2024-02-01 18:00:55 +0900

    feat : #로그인 김나혜 [240201] 로그인 페이지 및 카카오 토근값 저장

--- src/main/reactjs/src/components/login/LoginMain.js
+++ src/main/reactjs/src/components/login/LoginMain.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+
 const LoginMain = () => {
     return (
         <div>
-            <h2>로그인 ㅋ</h2>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <button className='btn btn-success'>네이버 로그인</button>
+            <br/><br/><br/><br/>
+            <button className='btn btn-warning'>카카오 로그인</button>
+            <br/><br/><br/><br/>
+            <button className='btn btn-secondary'>관리자 로그인</button>
         </div>
     );
 };
