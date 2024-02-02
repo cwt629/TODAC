daf5c97559e9abd7f0a04d851f1233bf72f2f586
Nahye <143480649+joyzoey@users.noreply.github.com>
2024-02-02 15:51:13 +0900

    feat : #login 김나혜 [240202] - (메인 로그인 & admin 로그인) 페이지 셋팅 - 백 보안 셋팅 위한 틀 생성 (+gradle)

--- src/main/reactjs/src/components/login/LoginMain.js
+++ src/main/reactjs/src/components/login/LoginMain.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 const LoginMain = () => {
+    const nav = useNavigate();
+
     return (
-        <div>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <button className='btn btn-success'>네이버 로그인</button>
+        <div style={{padding : "100px"}}>
+            TODAC
             <br/><br/><br/><br/>
-            <button className='btn btn-warning'>카카오 로그인</button>
-            <br/><br/><br/><br/>
-            <button className='btn btn-secondary'>관리자 로그인</button>
+            <button className='btn btn-success'
+             style={{width : "100%"}}>네이버 로그인</button>
+            <br/><br/>
+            <button className='btn btn-warning'
+             style={{width : "100%"}}>카카오 로그인</button>
+            <br/><br/><br/><hr/><br/><br/>
+            <button className='btn btn-secondary'
+                onClick={() => nav('admin')}
+                style={{width : "100%"}}>관리자 로그인</button>
         </div>
     );
 };
