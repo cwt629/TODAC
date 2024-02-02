bcf4f64cf60e807931d6c2212eb959d179c8e111
김동현 <kd0948@naver.com>
2024-02-02 11:42:52 +0900

    feat : #마이페이지 김동현 [240202] - 결제 버튼만 생성 -브랜치 생성용 커밋

--- src/main/reactjs/src/components/mypage/MypageMain.js
+++ src/main/reactjs/src/components/mypage/MypageMain.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 const MypageMain = () => {
     return (
         <div>
             <h3>마이페이지임 ㅋ</h3>
+            <Button variant="outlined">충전</Button>
         </div>
     );
 };
