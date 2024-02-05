b7893e8bf3a3f97b4fe3475da3000bae558b3287
Nahye <143480649+joyzoey@users.noreply.github.com>
2024-02-05 15:33:07 +0900

    feat : #로그인 김나혜 [240205] 세션스토리지-토큰,id - 로그인시 세션스토리지에 토큰,id 저장하기 - 로그아웃시 세션스토리지 지우기

--- src/main/reactjs/src/components/admin/AdminMain.js
+++ src/main/reactjs/src/components/admin/AdminMain.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
@@ -6,10 +6,17 @@ const AdminMain = () => {
 
     const handleLogout = () => {
         sessionStorage.removeItem("token");
+        sessionStorage.removeItem("id");
         nav('/login'); // 로그아웃 후 로그인 페이지로 이동
     };
     const nav = useNavigate();
 
+    useEffect(()=>{
+        const storedToken = sessionStorage.getItem("token");
+        const storedId = sessionStorage.getItem("id");
+        console.log("Stored t:", storedToken,", Stored id:", storedId);
+    },[]);
+    
     return (
         <div className='mx_30'>
             <div className='mt-1 fs_14'>
