875d50514b389a4975dfad8482292d5fbfb35d0c
wontae chang <cwt629@naver.com>
2024-02-05 17:32:59 +0900

    fix: #인공지능채팅 장원태 [240205] - input의 초기 상태값을 빈 문자열로 수정 - 채팅창의 높이가 반응형으로 맞춰지도록 변경

--- src/main/reactjs/src/components/chat/chattingroom/ChatRoomHeader.js
+++ src/main/reactjs/src/components/chat/chattingroom/ChatRoomHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChatRoomHeader = () => {
     return (
-        <div>
+        <div className='chatheader'>
             <div className='fs_14 fw_500 col_blue2'>
                 <span>TODAC 채팅</span> {'>'} <span>상담받기</span>
             </div>
