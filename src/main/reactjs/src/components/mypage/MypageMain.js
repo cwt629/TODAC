e2f2c2a8df7124d58544aed643852ca952d1dc6f
김동현 <kd0948@naver.com>
2024-02-06 20:43:15 +0900

    fix : #마이페이지 김동현 [240205] - 배포 경고 수정 MypageMain.js Checkout.js Success.js RouterMain.js useEffect랑 neveruse 등등 수정

--- src/main/reactjs/src/components/mypage/MypageMain.js
+++ src/main/reactjs/src/components/mypage/MypageMain.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './QnaStyle.css';
 import axios from "axios";
 
@@ -7,6 +7,7 @@ const MypageMain = () => {
     const [member, setmember] = useState([]);
     const nav = useNavigate();
     const storedId = sessionStorage.getItem("id");
+    const storedToken = sessionStorage.getItem("token");
 
     const getmember = () => {
         const url = "/member/list?userid=" + storedId;
@@ -18,9 +19,11 @@ const MypageMain = () => {
     }
 
     useEffect(() => {
+        const storedToken = sessionStorage.getItem("token");
         const storedId = sessionStorage.getItem("id");
         getmember();
-    }, [getmember]);
+        console.log("Stored t:", storedToken, ", Stored id:", storedId);
+    }, []);
 
     const handleLogout = () => {
         // 세션에서 토큰 제거
