20dfaec1cbadbc15a7e6f0f7e7ae7590b46ade6f
2Smean <136636643+2Smean@users.noreply.github.com>
2024-02-18 20:23:21 +0900

    feat : # 게시글 수정 삭제 기능 구현 이승민 [20240218]
    - 게시글 수정, 삭제 구현 하지만 모두가 가능함 관리자와 본인글만 수정 및 삭제 가능하도록 할것임

--- src/main/reactjs/src/routers/user/community/RouterBoard.js
+++ src/main/reactjs/src/routers/user/community/RouterBoard.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import BoardMain from "../../../components/community/board/BoardMain";
 import BoardForm from "../../../components/community/board/BoardForm";
 import BoardDetail from "../../../components/community/board/BoardDetail";
+import BoardUpdateForm from "../../../components/community/board/BoardUpdateForm";
 
 const RouterBoard = () => {
     return (
@@ -10,6 +11,7 @@ const RouterBoard = () => {
             <Route path='' element={<BoardMain />} />
             <Route path='form' element={<BoardForm />} />
             <Route path='detail/:boardcode' element={<BoardDetail />} />
+            <Route path='updateform/:boardcode' element={<BoardUpdateForm />} />
         </Routes>
     );
 };
