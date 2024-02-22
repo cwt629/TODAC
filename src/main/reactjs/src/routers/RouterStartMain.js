09a1ae8f86baeacbacb14ad934fb2d7b93cfcd2c
wontae chang <cwt629@naver.com>
2024-02-22 10:33:51 +0900

    fix: #footer 장원태 [240222] - footer가 일부 환경에서 페이지의 하단부를 가리는 현상 수정 - footer에 대한 align-self를 flex-end로 지정 - height: 100%였던 일부 페이지 main div에 대해 해당 스타일 해제

--- src/main/reactjs/src/routers/RouterStartMain.js
+++ src/main/reactjs/src/routers/RouterStartMain.js
@@ -37,24 +37,24 @@ const RouterStartMain = () => {
                 {/* <div className='appcontent startpage'> */}
                 <div className='startpage'>
 
-                <div className='backgrounds overlay'>
-                    <div className='background' data-aos='fade-in' data-aos-duration='1500' data-aos-anchor='.section--hero' style={{backgroundImage: `url(${background})`,backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center 87%'}}></div>
-                </div>
-
-                <header className='hero' data-aos='fade-in' data-aos-duration='1500'>
-                    <div className='hero-center'>
-                        <h1 className='hero__logo' data-aos='zoom-in'>TODAC</h1>
-                        <div className='hero__text' data-aos='fade-up' data-aos-easing='ease' data-aos-delay='400'>우리가 <span>궁금하신가요?</span></div>
+                    <div className='backgrounds overlay'>
+                        <div className='background' data-aos='fade-in' data-aos-duration='1500' data-aos-anchor='.section--hero' style={{ backgroundImage: `url(${background})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center 87%' }}></div>
                     </div>
-                    <span className='hero__scroll fw_500' data-aos='fade-up' data-aos-easing='ease' data-aos-delay='800' onClick={handleScroll}>
-                        궁금증 해결을 위해 스크롤<br />
-                        <i className='chevron bottom'></i>
-                    </span>
-                </header>
 
-                <section className='section section--code' data-aos='fade-up'>
-                    <div className='container'>
-                        <h2 className='section-title'>고민이 많으셨나요?</h2>
+                    <header className='hero' data-aos='fade-in' data-aos-duration='1500'>
+                        <div className='hero-center'>
+                            <h1 className='hero__logo' data-aos='zoom-in'>TODAC</h1>
+                            <div className='hero__text' data-aos='fade-up' data-aos-easing='ease' data-aos-delay='400'>우리가 <span>궁금하신가요?</span></div>
+                        </div>
+                        <span className='hero__scroll fw_500' data-aos='fade-up' data-aos-easing='ease' data-aos-delay='800' onClick={handleScroll}>
+                            궁금증 해결을 위해 스크롤<br />
+                            <i className='chevron bottom'></i>
+                        </span>
+                    </header>
+
+                    <section className='section section--code' data-aos='fade-up'>
+                        <div className='container'>
+                            <h2 className='section-title'>고민이 많으셨나요?</h2>
                             <div className='code code--small code--left' data-aos='fade-up'>
                                 1
                             </div>
@@ -84,7 +84,7 @@ const RouterStartMain = () => {
                             </div>
                         </div>
                     </section>
-                    </div>
+                </div>
 
                 <Footer />
             </div>
