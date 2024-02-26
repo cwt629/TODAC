d3bab4ce4d72d3dec6df5cdfcef9a014d31c5f5d
Nahye <143480649+joyzoey@users.noreply.github.com>
2024-02-26 10:59:20 +0900

    feat : #game 김나혜 [240226] -프론트에서 백으로 데이터보내게 수정

--- src/main/reactjs/src/components/community/game/GameMain.js
+++ src/main/reactjs/src/components/community/game/GameMain.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState, useRef  } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
+import axios from 'axios';
 
 const GameMain = () => {
     const navi = useNavigate();
     const usercode = sessionStorage.getItem('usercode');
 
     let urlSet = 'http://175.45.192.182:7777?usercode=' + usercode;
-    urlSet = 'http://localhost:7777?usercode=' + usercode;
+    //urlSet = 'http://localhost:7777?usercode=' + usercode;
     //console.log(' ======================= urlSet : ', urlSet);
 
     useEffect(() => {
         const receiveMessage = (event) => {
             // 이벤트 객체의 origin 속성을 사용하여 메시지의 출처를 확인
-            if (event.origin !== 'http://localhost:7777') return; // 원하는 출처 확인
+            //if(event.origin !== 'http://175.45.192.182:7777') return; // 원하는 출처 확인
             if(event.data.gameDone) {
                 const score = event.data.score;
                 const point = score >= 800 ? 100 : 10;
-                Swal.fire({
-                    title: `<span style="font-size: 20px;">축하합니다! ${score} 점수를 획득하여 ${point} 포인트가 적립되었습니다!</span>`,
-                    confirmButtonColor: '#FF7170',
-                    background: '#F9EAEB'
+                
+                const url = "/game/insertpoint";
+                axios.post(url, {
+                      score
+                    , usercode
+                }).then(res => {
+                    Swal.fire({
+                        title: `<span style="font-size: 20px;">축하합니다! ${score} 점수를 획득하여 ${point} 포인트가 적립되었습니다!</span>`,
+                        confirmButtonColor: '#FF7170',
+                        background: '#F9EAEB'
+                    });
+                }).catch(error => {
+                    console.error("Error fetching inquiry data:", error);
                 });
             }
             console.log('Message from iframe:', event.data);
