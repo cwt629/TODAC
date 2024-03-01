7515edd1c0b0588b917de69419346017d0216351
Wontae Chang <cwt629@naver.com>
2024-03-01 22:17:35 +0900

    design: #채팅디자인 장원태 [240301] - 포인트 관련 버튼에 TP로 단위 수정, 디자인을 요약본쪽과 맞춤

--- src/main/reactjs/src/components/chat/document/ChatSummary.js
+++ src/main/reactjs/src/components/chat/document/ChatSummary.js
@@ -261,7 +261,7 @@ const ChatSummary = () => {
                             :
                             <span>
                                 <span role="img" aria-label="info-icon" className="info-icon" style={{ cursor: 'pointer', position: 'absolute', top: 3.5, right: -24 }} onClick={handleInfoClick}><img src={info} alt='Info Image' style={{ width: '20px', height: '20px' }} /></span>
-                                <button className='deepblue long' onClick={goDiagnosis}>진단서 발급(500P)</button>
+                                <button className='deepblue long' onClick={goDiagnosis}>진단서 발급(500TP)</button>
                             </span>
                     }
 
