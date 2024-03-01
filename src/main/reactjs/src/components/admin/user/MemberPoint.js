5c81afe0c0565f5893edb52629994ec9a6690dde
sseoy <yseoyeon0915@gmail.com>
2024-03-02 02:38:57 +0900

    feat: #관리자페이지 유서연 [240302] - 회원포인트내역 검색기능 없애고 타입별 sort기능 구현

--- src/main/reactjs/src/components/admin/user/MemberPoint.js
+++ src/main/reactjs/src/components/admin/user/MemberPoint.js
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from "axios";
-import { Pagination, InputAdornment, OutlinedInput, Input, IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import ClearIcon from '@mui/icons-material/Clear';
+import { Pagination } from '@mui/material';
 import PageHeader from '../../PageHeader';
-
 
 const MemberPoint = () => {
     const nav = useNavigate();
     const [point, setPoint] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const usercode = params.get("usercode");
     const [member, setMember] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(4);
+    const [selectedType, setSelectedType] = useState("all");
 
     const CURRENT_ROUTES = [
         { name: '관리자 홈', url: '/admin' },
@@ -25,6 +22,7 @@ const MemberPoint = () => {
         { name: '회원 정보', url: `/admin/MemberManage/MemberProfile?usercode=${usercode}` },
         { name: '회원 TP 사용내역', url: '' }
     ];
+
     const PAGE_TITLE = "회원 TP 사용내역";
 
     useEffect(() => {
@@ -61,17 +59,23 @@ const MemberPoint = () => {
         setCurrentPage(value);
     };
 
-    // 검색어와 일치하는 포인트 사용내역만 필터링
-    const filteredPoint = point.filter((item) => item.type.includes(searchQuery));
+    const handleTypeChange = (event) => {
+        const newSelectedType = event.target.value;
+        setSelectedType(newSelectedType);
+        setCurrentPage(1); // 유형이 변경될 때 페이지를 첫 번째 페이지로 재설정
+    };
 
     // 날짜를 내림차순으로 정렬
-    const sortedPoint = filteredPoint.sort((a, b) => new Date(b.applieddate) - new Date(a.applieddate));
+    const sortedPoint = point.sort((a, b) => new Date(b.applieddate) - new Date(a.applieddate));
+
+    // 선택된 유형에 따라 데이터 필터링
+    const filteredPoint = selectedType === "all" ? sortedPoint : sortedPoint.filter(item => item.type === selectedType);
 
     // 페이징을 위한 계산
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = sortedPoint.slice(indexOfFirstItem, indexOfLastItem);
-    const totalPages = Math.ceil(sortedPoint.length / itemsPerPage);
+    const currentItems = filteredPoint.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(filteredPoint.length / itemsPerPage);
 
     return (
         <div className='mx_30'>
@@ -83,40 +87,30 @@ const MemberPoint = () => {
                 </div>
             </div>
             <br />
-            <Input
-                id="search"
-                type="text"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                placeholder="검색할 타입을 입력해주세요"
-                className="form-control mb-3 fs_16 fw_800"
-                style={{
-                    height: '40px',
-                    padding: '8px',
-                    borderBottom: '1px solid #D4E4F2',
-                    borderRadius: '0',
-                    border: 'none',
-                }}
-                startAdornment={
-                    <>
-                        {searchQuery && (
-                            <InputAdornment position="start">
-                                <IconButton onClick={() => setSearchQuery('')}>
-                                    <ClearIcon />
-                                </IconButton>
-                            </InputAdornment>
-                        )}
-                    </>
-                }
-                endAdornment={
-                    <InputAdornment position="end">
-                        <SearchIcon />
-                    </InputAdornment>
-                }
-            />
             <br /><br />
-            <div className="fs_17 fw_800">
-                <span className="col_blue2">{member.nickname}</span> 님의 TP 사용내역
+            <div className='fs_17 fw_800' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div>
+                    <span className='col_blue2'>{member.nickname}</span> 님의 TP 사용내역
+                </div>
+                <div style={{ textAlign: 'right' }}>
+                    <label htmlFor="typeSelect" className='dropdown-toggle white pressable' type='button' data-bs-toggle="dropdown">
+                        {selectedType === 'all' ? '전체' : selectedType}
+                    </label>
+                    <ul className='dropdown-menu' style={{ marginTop: '6px' }}>
+                        <li key="all" value="all" onClick={() => handleTypeChange({ target: { value: 'all' } })}>
+                            <a className={`dropdown-item ${selectedType === 'all' ? 'fw_600' : ''}`} href='#'
+                                style={{ color: (selectedType === 'all') ? 'var(--deepblue)' : 'var(--mainblack)' }}>전체</a>
+                        </li>
+                        {
+                            Array.from(new Set(sortedPoint.map(item => item.type))).map((type, index) => (
+                                <li key={index} value={type} onClick={() => handleTypeChange({ target: { value: type } })}>
+                                    <a className={`dropdown-item ${type === selectedType ? 'fw_600' : ''}`} href='#'
+                                        style={{ color: (type === selectedType) ? 'var(--deepblue)' : 'var(--mainblack)' }}>{type}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
             {filteredPoint.length === 0 ? (
                 <div className='fs_14' style={{ marginTop: '10px' }}>
@@ -124,17 +118,17 @@ const MemberPoint = () => {
                 </div>
             ) : (
                 currentItems.map((item, index) => (
-                    <div key={index} className="bg_gray bor_gray1 px-3 py-2" style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div key={index} className='bg_gray bor_gray1 px-3 py-2' style={{ display: 'flex', flexDirection: 'column' }}>
                         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div>
-                                <span className="fw_700">{item.applieddate}</span>
+                                <span className='fw_700'>{item.applieddate}</span>
                             </div>
                             <div>
-                                <span className="fs_15 w_500">{item.type}</span>
+                                <span className='fs_15 w_500'>{item.type}</span>
                             </div>
                         </div>
                         <div style={{ marginTop: '8px' }}>
-                            <span className="fw_600 col_blue2">{item.amount}</span>
+                            <span className='fw_600 col_blue2'>{item.amount}</span>
                             <span className='fw_600'> TP</span>
                         </div>
                     </div>
