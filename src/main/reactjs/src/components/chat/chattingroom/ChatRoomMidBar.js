7b8c75a17b867c74c3834a7b07f36c9ff4f018da
Wontae Chang <cwt629@naver.com>
2024-03-02 21:27:22 +0900

    design: #인공지능채팅 장원태 [240302] - midbar의 텍스트가 2줄이 되면 버튼도 같이 2줄이 되던 현상 수정

--- src/main/reactjs/src/components/chat/chattingroom/ChatRoomMidBar.js
+++ src/main/reactjs/src/components/chat/chattingroom/ChatRoomMidBar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ChatRoomMidBar = ({ counselorname, handleFinishChat }) => {
     return (
         <div className='fs_18 fw_600 mt_10 chatmid'>
-            <div>
+            <div className='chatmid-title'>
                 <span className='fs_19 col_blue2'>{counselorname ? counselorname : '어느'}</span> 상담사와 마음 공유중...
             </div>
             <button type='button' className='deepblue'
