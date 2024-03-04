e146002aff975822e9837d0acc6b2a341ce0858a
2Smean <136636643+2Smean@users.noreply.github.com>
2024-03-04 23:41:32 +0900

    etc : # Webcontroller 에서 "/board/detail" 삭제 이승민

--- src/main/java/mypage/controller/WebController.java
+++ src/main/java/mypage/controller/WebController.java
@@ -5,8 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    @GetMapping(value = {"", "/login/callback", "/login/logoutcallback",
-            "/user/point/success","/board/detail/*", "/user/chat","/board/*" })
+    @GetMapping(value = {"",
+            "/login/callback", "/login/logoutcallback","/login",
+            "/user","/user/chat","/user/chat/loglist","/user/chat/logcontent","/user/chat/summary","/user/chat/diagnosis",
+            "/user/chat/counsel","/user/chat/custom",
+            "/user/community/","/user/community/donation","/user/community/game",
+            "/board","/board/*" ,"/board/detail/*","/board/form","/board/updateform","/board/updateform/*",
+            "/user/point","/user/point/charge","/user/point/checkout","/user/point/success","/user/point/fail",
+            "/user/myboard","/user/donate","user/update","/user/badge","/user/inquiry","/user/inquiry/form",
+            "/user/inquiry/detail","/user/inquiry/detail/*",
+            "/user/faq",
+            "/admin","/login/admin",
+            "/admin/MemberManage","/admin/MemberManage/MemberProfile","/admin/MemberManage/MemberProfile/MemberPost",
+            "/admin/MemberManage/MemberProfile/MemberComment","/admin/MemberManage/MemberProfile/MemberPayment",
+            "/admin/MemberManage/MemberProfile/MemberPoint","/admin/MemberManage/MemberProfile/MemberChatSearch",
+            "/admin/InquiryHistory","/admin/InquiryHistory/InquiryHistoryDetail","/admin/InquiryHistory/InquiryHistoryDetail/*"})
     public String accountRoute() {
         return "forward:/index.html";
     }
